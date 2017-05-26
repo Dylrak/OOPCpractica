092a0dd6c1b636f35dff9c489b40dc85f3ba1bc7
Dylan-Laptop\Dylan <dylrak@hotmail.com>
2017-05-26 17:19:24 +0200

    Week 4 klaar

---
+++ week-4-patroon/skitt.cpp
@@ -0,0 +1,10 @@
+#include "skitt.hpp"
+
+skitt::skitt()
+{
+}
+
+skitt::~skitt()
+{
+}
+
---
+++ week-4-patroon/skitt.hpp
@@ -0,0 +1,12 @@
+#ifndef SKITT_HPP
+#define SKITT_HPP
+
+class skitt
+{
+public:
+	skitt();
+	~skitt();
+
+};
+
+#endif // SKITT_HPP
